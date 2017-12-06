0241adaeefb7023f69e0ebd71ec186a2b14f5138
mutouji <59022665@qq.com>
2017-12-06 15:36:03 +0800

    first commit

---
+++ taotao-manager/taotao-manager-web/src/test/java/com/taotao/controller/TestPageHelper.java
@@ -0,0 +1,16 @@
+package com.taotao.controller;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+
+import com.taotao.mapper.TbItemMapper;
+
+public class TestPageHelper {
+
+	@Test
+	public void testPageHelper() {
+		// create spring container
+//		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:");
+//		TbItemMapper mapper = applicationContext.getBean(TbItemMapper.class);
+	}
+}
